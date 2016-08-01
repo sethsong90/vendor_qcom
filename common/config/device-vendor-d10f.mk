840d7f7d78952bf868f1ca615a1bd051709d3db6
remittor <remittor@gmail.com>
2016-08-01 10:56:14 +0300

    common: config: Transfer device specific libs to individual config

---
+++ common/config/device-vendor-d10f.mk
@@ -0,0 +1,59 @@
+MM_CAMERA += libmmcamera_imx135
+MM_CAMERA += libchromatix_imx135_common
+MM_CAMERA += libchromatix_imx135_default_video
+MM_CAMERA += libchromatix_imx135_hfr_60
+MM_CAMERA += libchromatix_imx135_hfr_90
+MM_CAMERA += libchromatix_imx135_hfr_120
+MM_CAMERA += libchromatix_imx135_liveshot
+MM_CAMERA += libchromatix_imx135_preview
+MM_CAMERA += libchromatix_imx135_snapshot
+MM_CAMERA += libchromatix_imx135_video_hd
+MM_CAMERA += libchromatix_imx135_zsl
+
+MM_CAMERA += libmmcamera_ov2720
+MM_CAMERA += libchromatix_ov2720_common
+MM_CAMERA += libchromatix_ov2720_default_video
+MM_CAMERA += libchromatix_ov2720_hfr
+MM_CAMERA += libchromatix_ov2720_liveshot
+MM_CAMERA += libchromatix_ov2720_preview
+MM_CAMERA += libchromatix_ov2720_zsl
+
+MM_CAMERA += libmmcamera_skuf_ov5648_p5v23c
+MM_CAMERA += libmmcamera_sunny_p5v23c_eeprom
+MM_CAMERA += libchromatix_skuf_ov5648_p5v23c_common
+MM_CAMERA += libchromatix_skuf_ov5648_p5v23c_default_video
+MM_CAMERA += libchromatix_skuf_ov5648_p5v23c_preview
+MM_CAMERA += libchromatix_skuf_ov5648_p5v23c_snapshot
+
+MM_CAMERA += libmmcamera_ov13850
+MM_CAMERA += libmmcamera_sunny_q13v03a_eeprom
+MM_CAMERA += libchromatix_ov13850_common
+MM_CAMERA += libchromatix_ov13850_preview
+MM_CAMERA += libchromatix_ov13850_default_video
+MM_CAMERA += libchromatix_ov13850_hfr
+MM_CAMERA += libchromatix_ov13850_liveshot
+MM_CAMERA += libchromatix_ov13850_hfr_60fps
+MM_CAMERA += libchromatix_ov13850_hfr_90fps
+MM_CAMERA += libchromatix_ov13850_hfr_120fps
+MM_CAMERA += libchromatix_ov13850_snapshot
+MM_CAMERA += libchromatix_ov13850_zsl
+MM_CAMERA += libchromatix_ov13850_video_hd
+
+MM_CAMERA += libmmcamera_t4k37ab
+MM_CAMERA += libmmcamera_f4k37ab_qtech_t4k37_eeprom
+MM_CAMERA += libchromatix_t4k37ab_common
+MM_CAMERA += libchromatix_t4k37ab_preview
+MM_CAMERA += libchromatix_t4k37ab_default_video
+MM_CAMERA += libchromatix_t4k37ab_snapshot
+
+MM_CAMERA += libmmcamera_imx214
+MM_CAMERA += libmmcamera_sony_imx214_eeprom
+MM_CAMERA += libchromatix_imx214_common
+MM_CAMERA += libchromatix_imx214_preview
+MM_CAMERA += libchromatix_imx214_default_video
+MM_CAMERA += libchromatix_imx214_liveshot
+MM_CAMERA += libchromatix_imx214_hfr_60fps
+MM_CAMERA += libchromatix_imx214_hfr_120fps
+MM_CAMERA += libchromatix_imx214_snapshot
+MM_CAMERA += libchromatix_imx214_snapshot_hdr
+MM_CAMERA += libchromatix_imx214_video_hdr
